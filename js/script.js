d7f71984b684cd13f1b8855c5469564399f907df
Valentin Faguet <valentin@wallmatt.com>
2021-11-17 14:39:32 +0100

    Importation des fichiers originaux

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.carousel').carousel();
+}); 
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+
+//   $(".col").hover(function() {
+//     $(this).next('.redirect-button').fadeIn();
+//   }, function() {
+//     $(this).next('.redirect-button').fadeOut();
+//   });
